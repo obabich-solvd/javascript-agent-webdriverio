d14b4b3a7f6e05000c4a55ab784980890244cf8d
obabich-solvd <71430326+obabich-solvd@users.noreply.github.com>
2023-02-27 15:25:43 +0300

    fixes

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,44 +1,42 @@
 const log = require("loglevel")
-const { currentTest } = require("@zebrunner/javascript-agent-webdriverio")
+const { currentTest } = require("@zebrunner/javascript-agent-webdriverio");
+const { Key } = require("webdriverio");
 
 describe('Basic reporting test', () => {
 
-    const logger = log.getLogger("LOGGER")
-
+    const logger = log.getLogger("LOGGER");
     const url = 'https://www.google.com/';
-    const cookiesDialogText = 'Before you continue to Google Search';
-    const searschValue = 'Zebrunner'
+    const searchValue = 'Zebrunner';
 
     before(function () {
-        logger.warn('this log message will not be submitted into Zebrunner')
-         browser.maximizeWindow();
+        logger.warn('This log message will not be submitted into Zebrunner');
+        browser.maximizeWindow();
 
     });
 
     beforeEach(function () {
-        logger.info(` Test ${this.currentTest.title} satrted `)
+        logger.info(`Test ${this.currentTest.title} started`);
     });
 
     it('Google search', async () => {
-       
-        logger.info(`Opening ${url}`)
-        await browser.url(url)
-        currentTest.saveScreenshot(await browser.takeScreenshot())
 
-        logger.info(`Performing search with value: ${searschValue}`)
-        await $("//input[@name='q']").setValue(searschValue)
-        await browser.keys('Enter')
+        logger.info(`Opening ${url}`);
+        await browser.url(url);
+        currentTest.saveScreenshot(await browser.takeScreenshot());
 
-        logger.info(`Verify first search result contains ${searschValue}`)
-        currentTest.saveScreenshot(await browser.takeScreenshot())
-        await expect($("//*[@id='search']//a")).toHaveTextContaining(searschValue)
-        currentTest.attachArtifactReference("Zebrunner","https://zebrunner.com/")
+        logger.info(`Performing search with value: ${searchValue}`);
+        await $("//input[@name='q']").setValue(searchValue);
+        await browser.keys(Key.Enter);
 
+        logger.info(`Verify first search result contains ${searchValue}`);
+        currentTest.saveScreenshot(await browser.takeScreenshot());
+
+        await expect($("//*[@id='search']//a")).toHaveTextContaining(searchValue);
     })
 
     afterEach(function () {
-        currentTest.saveScreenshot(browser)
-        logger.info(` Test  ${this.currentTest.title} finished  as ${this.currentTest.state}`)
+        currentTest.saveScreenshot(browser);
+        logger.info(`Test ${this.currentTest.title} finished as ${this.currentTest.state}`);
     });
 })
 
